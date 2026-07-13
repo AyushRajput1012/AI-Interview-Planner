6c8118047d485b9bd3a8f8794ffa456a3b3980b8
AYUSHRAJPUT1012 <ayushrajpoot9670@gmail.com>
2026-07-13 23:31:56 +0530

    Fix backend deployment for Render

--- Backend_GEN-AI/server.js
+++ Backend_GEN-AI/server.js
@@ -1,18 +1,15 @@
-require('dotenv').config()
-const app = require('./src/app')
-const connectToDB = require('./src/config/database')
-const dns = require('dns')
+require("dotenv").config();
 
+const app = require("./src/app");
+const connectToDB = require("./src/config/database");
+const dns = require("dns");
 
 dns.setServers(["1.1.1.1", "8.8.8.8"]);
 
-
 connectToDB();
 
+const PORT = process.env.PORT || 3000;
 
-
-
-
-app.listen(3000, () =>{
-    console.log('Server is running on port 3000')
-})
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
